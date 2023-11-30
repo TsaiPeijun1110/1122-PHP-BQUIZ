b076af7472960a96f992d66f7f67578033da789f
tsai <lovez81427@gmail.com>
2023-11-30 16:07:23 +0800

    完成編輯文字及設定是否顯示的功能

--- q1/edit_title .php
+++ q1/edit_title .php
@@ -1,11 +1,34 @@
 <?php
-function dd($array)
-{
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
+include_once "db.php";
+
+foreach ($_POST['id'] as $key => $id) {
+    $row = $Title->find($id);
+    //dd($row);
+    $row['text'] = $_POST['text'][$key];
+    //dd($row);
+    $Title->save($row);
+}
+
+foreach ($_POST['id'] as $id) {
+    $row = $Title->find($id);
+    //下面這五行程式碼是原始本的
+    // if($id==$_POST['sh']){
+    //         $row['sh']=1;
+    // }else{
+    //     $row['sh']=0;
+    // }
+    $row['sh']=($id==$_POST['sh'])?1:0;   //這段式簡化過的程式碼
+    $Title->save($row);
 }
 
 
-$Title=new DB('titles');
-?>
+
+// function dd($array)
+// {
+//     echo "<pre>";
+//     print_r($array);
+//     echo "</pre>";
+// }
+
+
+// $Title=new DB('titles');
