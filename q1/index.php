3fb8011146d1385f4202da8758e58a77db6d9d93
tsai <lovez81427@gmail.com>
2023-11-30 14:50:53 +0800

    建立彈出視窗檔案view.php & 修改modal載入路徑,建立資料庫 & 完成新增標題資料功能 & 完成管理列表資料功能

--- q1/index.php
+++ q1/index.php
@@ -1,3 +1,4 @@
+<?php include_once "db.php" ;?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>題組一</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/css.css">
 </head>
 <body>
+<div id="cover" style="display:none; ">
+	<div id="coverr">
+    	<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('#cover')">X</a>
+        <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
+    </div>
+</div>
     <header class="container">
         <img src="" alt="">
     </header>
@@ -23,16 +31,23 @@
                 <td>刪除</td>
                 <td></td>
             </tr>
+            <?php
+            $rows=$Title->all();
+            foreach($rows as $row){
+            ?>
             <tr>
-                <td><img src="" style="width:300px;height:30px"></td>
-                <td><input type="text" name="" id="" style="width:90%"></td>
+                <td><img src="./img/<?=$row['img'];?>" style="width:300px;height:30px"></td>
+                <td><input type="text" name="" id="" value="<?=$row['text'];?>" style="width:90%"></td>
                 <td><input type="radio" name="" id=""></td>
                 <td><input type="checkbox" name="" id=""></td>
-                <td><input type="btn btn-primary" type="button" value="更新圖片"></td>
+                <td><input class="btn btn-primary" type="button" value="更新圖片"></td>
             </tr>
+            <?php
+             }
+            ?>
         </table>
         <div class="d-flex justify-content-between">
-            <div><input type="button" value="新增網頁標題圖片"></div>
+            <div><input type="button" onclick="op('#cover','#cvr','title.php?do=title')" value="新增網頁標題圖片"></div>
         <div>
         <input type="submit" value="修改確定">
         <input type="reset" value="重置">
@@ -41,7 +56,8 @@
         </div>
         </form>
     </main>
-    
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/js.js"></script>
     <script src="../js/bootstrap.js"></script>
 </body>
 
