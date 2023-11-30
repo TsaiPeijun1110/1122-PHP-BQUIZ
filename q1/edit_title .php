faa55b0ca16a9f55cc50853a0bf397f4c9728a84
tsai <lovez81427@gmail.com>
2023-11-30 16:39:42 +0800

    完成前台讀取設為顯示的標題圖片功能

--- q1/edit_title .php
+++ q1/edit_title .php
@@ -2,24 +2,44 @@
 include_once "db.php";
 
 foreach ($_POST['id'] as $key => $id) {
-    $row = $Title->find($id);
-    //dd($row);
-    $row['text'] = $_POST['text'][$key];
-    //dd($row);
-    $Title->save($row);
-}
 
-foreach ($_POST['id'] as $id) {
-    $row = $Title->find($id);
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+        $Title->del($id);
+    } else {
+        $row = $Title->find($id);
+        $row['text'] = $_POST['text'][$key];
+        $row['sh'] = ($id == $_POST['sh']) ? 1 : 0;
+        $Title->save($row);
+    }
+}
+header("location:index.php");
+
+
+
+
+
+
+
+
+//     $row = $Title->find($id);
+//     //dd($row);
+//     $row['text'] = $_POST['text'][$key];
+//     //dd($row);
+//     $Title->save($row);
+
+
+// foreach ($_POST['id'] as $id) {
+//     $row = $Title->find($id);
+
     //下面這五行程式碼是原始本的
     // if($id==$_POST['sh']){
     //         $row['sh']=1;
     // }else{
     //     $row['sh']=0;
     // }
-    $row['sh']=($id==$_POST['sh'])?1:0;   //這段式簡化過的程式碼
-    $Title->save($row);
-}
+    // $row['sh']=($id==$_POST['sh'])?1:0;   //這段式簡化過的程式碼
+    // $Title->save($row);
+// }
 
 
 
