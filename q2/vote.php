85234854a443eeab056e6c343dc99e081c2d3eaa
tsai <lovez81427@gmail.com>
2023-12-01 14:58:41 +0800

    完成投票紀錄功能

--- q2/vote.php
+++ q2/vote.php
@@ -19,7 +19,7 @@
         $subject=$Que->find($_GET['id']);
         ?>
         <h2 class="text-center"><?=$subject['text'];?></h2>
-        <form action="add_vote.php" method="post">
+        <form action="./api/vote.php" method="post">
         <ul class="list-group col-6 mx-auto">
             <?php
             $opts=$Que->all(['subject_id'=>$_GET['id']]);
@@ -34,7 +34,7 @@
             ?>
         </ul>
         <input type="submit" value="我要投票" class="btn- btn-primary d-block mx-auto my-5">
-        </form>
+    </form>
     </main>
     <script src="../js/jquery-3.4.1.min.js"></script>
     <script src="../js/bootstrap.js"></script>
